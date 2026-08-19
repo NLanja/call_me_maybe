b60b9aebb3debf73ce48e9059394cba043fa9e35
Lanja Nasaina <lanasain@c1r5s4.42antananarivo.mg>
2026-08-19 17:58:25 +0300

    ajout argparse et test LLM

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import argparse
+
 os.environ['HF_HOME'] = '/goinfre/lanasain/.cache/huggingface'
 
 from llm_sdk import Small_LLM_Model
@@ -10,22 +12,35 @@ from .config_parse import (
 
 
 def main():
-    check = Small_LLM_Model()
-    try:
-        functions = load_function_definitions(
-            "data/input/functions_definition.json"
-            )
-        print(functions)
-        print()
-        test = load_function_test("")
-        print(test)
-        for i in test:
-            print(i)
+    parser = argparse.ArgumentParser(description = "data")
+    parser.add_argument("--functions_definition", type=str, required=False, help="Function definition")
+    parser.add_argument("--prompt", type=str, required=False, help="Prompt")
+    args = parser.parse_args()
+    print(args.functions_definition)
 
-    except JsonFileError as error:
-        print(f"Error: {error}")
-        sys.exit(1)
+#     check = Small_LLM_Model()
+#     list_fun = load_function_definitions("")
+#     list_prompt = load_function_test("")
+#     lists = []
+#     for a in range(len(list_fun)):
+#         lists.append(list_fun[a].name)
+#     # print(lists)
 
+#     for i in range(len(list_prompt)):
+#         a = check.encode(f"Here is my prompt '{list_prompt[i].prompt}' and the list of functions '{lists}'; just give me the name of the function that corresponds to my prompt in the list of functions.")
+#         encode = a[0].tolist()
+#         lenght = len(encode)
+
+#         num_tokens_to_generate = 10
+
+#         for _ in range(num_tokens_to_generate):
+#             logit = check.get_logits_from_input_ids(encode)
+#             response = max(range(len(logit)), key=lambda i: logit[i])
+#             encode.append(response)
+
+#         deco = check.decode(encode[lenght:])
+#         print(deco)
+#         print(list_prompt[i].prompt)
 
 if __name__ == "__main__":
     main()
