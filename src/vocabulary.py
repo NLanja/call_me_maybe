f37bea3efc73d04d510450f7cf731f66401cc091
Lanja Nasaina <lanasain@c1r3s4.42antananarivo.mg>
2026-08-24 17:43:19 +0300

    load vocabulaire dans quen

---
+++ src/vocabulary.py
@@ -0,0 +1,20 @@
+from typing import Any
+from .config_parse import load_json_file
+
+
+def load_vocabulary(model: Any) -> tuple[dict[int, str], set[int]]:
+    vocab_path = model.get_path_to_vocab_file()
+    vocab = load_json_file(vocab_path)
+    id_to_token = {token_id: token for token, token_id in vocab.items()}
+
+    tokenizer_path = model.get_path_to_tokenizer_file()
+    tokenizer = load_json_file(tokenizer_path)
+    added_tokens = tokenizer["added_tokens"]
+    special_ids: set[int] = set()
+
+    for token in added_tokens:
+        id_to_token[token["id"]] = token["content"]
+        if token["special"]:
+            special_ids.add(token["id"])
+
+    return id_to_token, special_ids
